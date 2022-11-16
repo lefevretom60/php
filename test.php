ed1dde065736d77e685c9919e47241292a219ed0
lefevretom60 <tomlefevre60@gmail.com>
2022-11-16 17:17:57 +0100

    first commit

---
+++ test.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+       $tableau = array();
+       $tableau[0]=> '2';
+       $tableau [1]=>'5';
+       $tableau [2]=> '8';
+       $tableau [3]=> '4';
+       $tableau [4]=> '6';
+       $tableau [5]=> '9';
+      
+ 
+        ?>
+        <div>
+            <?php
+            echo $tableau[3];
+            ?>
+        </div>
+ 
+ 
+   
+   
+</body>
+</html>
+
